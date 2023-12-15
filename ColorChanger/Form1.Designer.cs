1342d2e0cdf8bb1b7d61643d26658f2207dc68ed
simas <simas@LOL-TU-BIRKELES>
2023-12-15 23:43:57 +0200

    Add project files.

---
+++ ColorChanger/Form1.Designer.cs
@@ -0,0 +1,370 @@
+﻿namespace ColorChanger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LoadXAML = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ConvertToHex = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SavePresetButton = new System.Windows.Forms.Button();
+            this.LoadPresetButton = new System.Windows.Forms.Button();
+            this.PresetTextBox = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.fuckoff = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.AdvancedCheckBox = new System.Windows.Forms.CheckBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.FullOpen = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.YellowGreen;
+            this.button1.Location = new System.Drawing.Point(839, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(222, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Color Selector";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(964, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // LoadXAML
+            // 
+            this.LoadXAML.BackColor = System.Drawing.Color.LightCyan;
+            this.LoadXAML.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoadXAML.ForeColor = System.Drawing.Color.Black;
+            this.LoadXAML.Location = new System.Drawing.Point(12, 46);
+            this.LoadXAML.Name = "LoadXAML";
+            this.LoadXAML.Size = new System.Drawing.Size(372, 52);
+            this.LoadXAML.TabIndex = 2;
+            this.LoadXAML.Text = "Set selected as PrimaryColor";
+            this.LoadXAML.UseVisualStyleBackColor = false;
+            this.LoadXAML.Click += new System.EventHandler(this.LoadXAML_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(882, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Selected:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(836, 557);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // ConvertToHex
+            // 
+            this.ConvertToHex.Location = new System.Drawing.Point(994, 557);
+            this.ConvertToHex.Name = "ConvertToHex";
+            this.ConvertToHex.Size = new System.Drawing.Size(75, 20);
+            this.ConvertToHex.TabIndex = 7;
+            this.ConvertToHex.Text = "Convert";
+            this.ConvertToHex.UseVisualStyleBackColor = true;
+            this.ConvertToHex.Click += new System.EventHandler(this.ConvertToHex_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(832, 525);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(162, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Convert hex to colour:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(835, 581);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(153, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "(example: #FF80FF)";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 604);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(231, 109);
+            this.listBox1.TabIndex = 10;
+            this.listBox1.Visible = false;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(6, 577);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(231, 24);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "DefaultTheme.Colors vars:";
+            this.label6.Visible = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.panel1.Location = new System.Drawing.Point(839, 207);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(351, 217);
+            this.panel1.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(835, 175);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(107, 20);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Color preview:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(240, 557);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(99, 17);
+            this.checkBox1.TabIndex = 14;
+            this.checkBox1.Text = "Use as variable";
+            this.checkBox1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Visible = false;
+            // 
+            // SavePresetButton
+            // 
+            this.SavePresetButton.Location = new System.Drawing.Point(969, 670);
+            this.SavePresetButton.Name = "SavePresetButton";
+            this.SavePresetButton.Size = new System.Drawing.Size(100, 23);
+            this.SavePresetButton.TabIndex = 15;
+            this.SavePresetButton.Text = "Save preset";
+            this.SavePresetButton.UseVisualStyleBackColor = true;
+            this.SavePresetButton.Click += new System.EventHandler(this.SavePresetButton_Click);
+            // 
+            // LoadPresetButton
+            // 
+            this.LoadPresetButton.Location = new System.Drawing.Point(1079, 671);
+            this.LoadPresetButton.Name = "LoadPresetButton";
+            this.LoadPresetButton.Size = new System.Drawing.Size(123, 23);
+            this.LoadPresetButton.TabIndex = 16;
+            this.LoadPresetButton.Text = "Load preset from file";
+            this.LoadPresetButton.UseVisualStyleBackColor = true;
+            this.LoadPresetButton.Click += new System.EventHandler(this.LoadPresetButton_Click);
+            // 
+            // PresetTextBox
+            // 
+            this.PresetTextBox.Location = new System.Drawing.Point(836, 672);
+            this.PresetTextBox.Name = "PresetTextBox";
+            this.PresetTextBox.Size = new System.Drawing.Size(128, 20);
+            this.PresetTextBox.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(832, 650);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(103, 20);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Preset name:";
+            // 
+            // fuckoff
+            // 
+            this.fuckoff.AutoSize = true;
+            this.fuckoff.Location = new System.Drawing.Point(893, 700);
+            this.fuckoff.Name = "fuckoff";
+            this.fuckoff.Size = new System.Drawing.Size(0, 13);
+            this.fuckoff.TabIndex = 19;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(293, 604);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(241, 108);
+            this.listBox2.TabIndex = 20;
+            this.listBox2.Visible = false;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(330, 577);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(105, 24);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "CCLib vars:";
+            this.label9.Visible = false;
+            // 
+            // AdvancedCheckBox
+            // 
+            this.AdvancedCheckBox.AutoSize = true;
+            this.AdvancedCheckBox.Location = new System.Drawing.Point(12, 525);
+            this.AdvancedCheckBox.Name = "AdvancedCheckBox";
+            this.AdvancedCheckBox.Size = new System.Drawing.Size(104, 17);
+            this.AdvancedCheckBox.TabIndex = 22;
+            this.AdvancedCheckBox.Text = "Advanced mode";
+            this.AdvancedCheckBox.UseVisualStyleBackColor = true;
+            this.AdvancedCheckBox.CheckedChanged += new System.EventHandler(this.AdvancedCheckBox_CheckedChanged);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(35, 175);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(165, 25);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "Editable values:";
+            // 
+            // listBox3
+            // 
+            this.listBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 20;
+            this.listBox3.Location = new System.Drawing.Point(40, 216);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(203, 264);
+            this.listBox3.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(1275, 759);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.AdvancedCheckBox);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.fuckoff);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.PresetTextBox);
+            this.Controls.Add(this.LoadPresetButton);
+            this.Controls.Add(this.SavePresetButton);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ConvertToHex);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.LoadXAML);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Color Picker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button LoadXAML;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button ConvertToHex;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button SavePresetButton;
+        private System.Windows.Forms.Button LoadPresetButton;
+        private System.Windows.Forms.TextBox PresetTextBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label fuckoff;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.CheckBox AdvancedCheckBox;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ListBox listBox3;
+    }
+}
+
---
+++ ColorChanger/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xaml;
+using System.Xml;
+using System.Xml.Linq;
+using System.IO.Compression;
+using System.Reflection;
+
+namespace ColorChanger
+{
+    public partial class Form1 : Form
+    {
+       
+        Color currentcolor;
+        int mapindex = -1;
+        string selectedfile = "";
+        XmlDocument docc = new XmlDocument();
+        XmlDocument cclib = new XmlDocument();
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "#000000";
+            currentcolor = colorDialog1.Color;
+            docc.Load("DefaultTheme.Colours.xaml");
+            cclib.Load("CCLib.xaml");
+            for (int i = 0; i < docc.ChildNodes[0].ChildNodes.Count; i++)
+            {
+                listBox1.Items.Add(docc.ChildNodes[0].ChildNodes[i].Attributes[0].InnerText);
+            }
+            for (int i = 0; i < cclib.ChildNodes[0].ChildNodes.Count; i++)
+            {
+                if (cclib.ChildNodes[0].ChildNodes[i].Name == "SolidColorBrush")
+                {
+                    listBox2.Items.Add(cclib.ChildNodes[0].ChildNodes[i].Attributes[0].Value);
+                }
+                
+            }
+            System.IO.Directory.CreateDirectory("CustomPresets");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                label2.Text = "";
+                var r = colorDialog1.Color.R;
+                var g = colorDialog1.Color.G;
+                var b = colorDialog1.Color.B;
+                var hexcolor = string.Format("{0:X2}{1:X2}{2:X2}", r, g, b);
+                label1.Text = "#" + hexcolor.ToString();
+                label1.ForeColor = colorDialog1.Color;
+                currentcolor = colorDialog1.Color;
+                panel1.BackColor = currentcolor;
+
+            }
+        }
+
+        private void LoadXAML_Click(object sender, EventArgs e)
+        {
+            
+            
+            if (mapindex == -1)
+            {
+                docc.ChildNodes[0].ChildNodes[32].ChildNodes[0].Value = label1.Text;
+            }
+            else
+            {
+                if (selectedfile == "DefaultTheme.Colours.xaml")
+                {
+                    if (docc.ChildNodes[0].ChildNodes[mapindex].Name == "SolidColorBrush")
+                    {
+                        if (docc.ChildNodes[0].ChildNodes[mapindex].ChildNodes.Count == 0)
+                        {
+                            if (docc.ChildNodes[0].ChildNodes[mapindex].Attributes.Count == 3)
+                            {
+                                docc.ChildNodes[0].ChildNodes[mapindex].Attributes[2].Value = label1.Text;
+                            }
+                            else docc.ChildNodes[0].ChildNodes[mapindex].Attributes[1].Value = label1.Text;
+                        }
+                        else
+                        {
+                            docc.ChildNodes[0].ChildNodes[mapindex].ChildNodes[0].Value = label1.Text;
+                        }
+
+                    }
+                    else docc.ChildNodes[0].ChildNodes[mapindex].ChildNodes[0].Value = label1.Text;
+                }
+                if (selectedfile == "CCLib.xaml")
+                {
+                    cclib.ChildNodes[0].ChildNodes[mapindex].Attributes[1].Value = label1.Text;
+                }
+
+
+
+            }
+            if (selectedfile == "DefaultTheme.Colours.xaml")
+            {
+                docc.Save("DefaultTheme.Colours.xaml");
+            }
+            if (selectedfile == "CCLib.xaml")
+            {
+                cclib.Save("CCLib.xaml");
+            }
+
+            label2.Text = "Color for " + listBox1.Text + " changed successfully!";
+            label2.ForeColor = currentcolor;
+
+
+
+
+        }
+
+        private void ConvertToHex_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Color fromhex = ColorTranslator.FromHtml(textBox1.Text);
+                label1.Text = textBox1.Text;
+                label1.ForeColor = fromhex;
+                panel1.BackColor = fromhex;
+            }
+            catch
+            {
+                label4.Text = "Failed to convert. Make sure your hex is accurate!";
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                selectedfile = "DefaultTheme.Colors.xaml";
+                if (listBox2.SelectedIndex != -1) { listBox2.SelectedIndex = -1; }
+            }
+            
+
+            if (!checkBox1.Checked)
+            {
+                LoadXAML.Text = "Set selected color as " + listBox1.Text;
+                mapindex = listBox1.SelectedIndex;
+            }
+            else
+            {
+                LoadXAML.Text = "Set selected to variable " + listBox1.Text;
+                mapindex = listBox1.SelectedIndex;
+                label1.Text = "{StaticResource " + listBox1.Text + "}";
+                label1.ForeColor = Color.Black;
+            }
+
+        }
+
+        private void SavePresetButton_Click(object sender, EventArgs e)
+        {
+            if (PresetTextBox.Text == "")
+            {
+                MessageBox.Show("You need a name for your preset!");
+            }
+            else
+            {
+                using (ZipArchive zip = ZipFile.Open("CustomPresets\\" + PresetTextBox.Text + ".zip", ZipArchiveMode.Create))
+                {
+                    zip.CreateEntryFromFile("DefaultTheme.Colours.xaml", "DefaultTheme.Colours.xaml");
+                    zip.CreateEntryFromFile("CCLib.xaml", "CCLib.xaml");
+                    fuckoff.Text = "Saved preset " + PresetTextBox.Text + " to CustomPresets folder.";
+                }
+            }
+        }
+
+        private void LoadPresetButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory + "CustomPresets";
+            DialogResult res = ofd.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                using (ZipArchive zip = ZipFile.Open(ofd.FileName, ZipArchiveMode.Read))
+                    foreach (ZipArchiveEntry entry in zip.Entries)
+                    {
+                        if (entry.Name == "DefaultTheme.Colours.xaml")
+                        {
+                            if (File.Exists("DefaultTheme.Colours.xaml"))
+                            {
+                                File.Delete("DefaultTheme.Colours.xaml");
+                            }
+                            entry.ExtractToFile("DefaultTheme.Colours.xaml");
+
+                        }
+                        if (entry.Name == "CCLib.xaml")
+                        {
+                            if (File.Exists("CCLib.xaml"))
+                            {
+                                File.Delete("CCLib.xaml");
+                            }
+                            entry.ExtractToFile("CCLib.xaml");
+
+                        }
+                    }
+
+                fuckoff.Text = "Loaded preset successfully!";
+            }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedIndex != -1)
+            {
+                selectedfile = "CCLib.xaml";
+                if (listBox1.SelectedIndex != -1) { listBox1.SelectedIndex = -1; }
+                mapindex = listBox2.SelectedIndex;
+            }
+
+            if (!checkBox1.Checked)
+            {
+                LoadXAML.Text = "Set selected color as " + listBox2.Text;
+                mapindex = listBox2.SelectedIndex;
+            }
+            else
+            {
+                LoadXAML.Text = "Set selected to variable " + listBox2.Text;
+                mapindex = listBox2.SelectedIndex;
+                label1.Text = "{StaticResource " + listBox2.Text + "}";
+                label1.ForeColor = Color.Black;
+            }
+        }
+
+        private void AdvancedCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (AdvancedCheckBox.Checked)
+            {
+                label6.Visible = true;
+                label9.Visible = true;
+                listBox1.Visible = true;
+                listBox2.Visible = true;
+                checkBox1.Visible = true;
+            }
+            else
+            {
+                label6.Visible = false;
+                label9.Visible = false;
+                listBox1.Visible = false;
+                listBox2.Visible = false;
+                checkBox1.Visible = false;
+            }
+        }
+    }
+}
